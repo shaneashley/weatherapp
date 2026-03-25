e63c8efdae8202d6475380a5348f7a40c4a5ee41
Ashley <78882505+shaneashley@users.noreply.github.com>
2026-03-25 21:08:52 +0000

    Update CurrentWeather.jsx
    fixed images issues

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,7 +1,7 @@
 const CurrentWeather = ({ currentWeather }) => {
     return (
       <div className="current-weather">
-        <img src={`icons/${currentWeather.weatherIcon}.svg`} className="weather-icon" />
+      <img src={`/icons/${currentWeather.weatherIcon}.svg`} className="weather-icon" />
         <h2 className="temperature">
           {currentWeather.temperature} <span>°C</span>
         </h2>
