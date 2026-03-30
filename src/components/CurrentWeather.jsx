dca2011f7182652cd528157eb3c369ac266772ab
Ashley <78882505+shaneashley@users.noreply.github.com>
2026-03-30 18:23:09 +0100

    Update CurrentWeather.jsx

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,7 +1,8 @@
 const CurrentWeather = ({ currentWeather }) => {
     return (
       <div className="current-weather">
-      <img src="{`icons/${currentWeather.weatherIcon}.svg`} className="weather-icon" />
+          <img src="/icons/${currentWeather.weatherIcon}.svg`} className="weather-icon" alt="Icon" />
+
         <h2 className="temperature">
           {currentWeather.temperature} <span>°C</span>
         </h2>
